16b6002abcfc28b9bf06ea867e8cef908f743f12
Heena-Sangtrash <134187183+Heena-Sangtrash@users.noreply.github.com>
2023-05-20 22:23:24 -0700

    Add files via upload

---
+++ Snakegame/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        new snakeframe();
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+
+
+    }
+}
